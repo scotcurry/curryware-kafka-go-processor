6b607f2fcf1960a0b68f54485f5f584537b34bc9
Scot Curry <scotcurry4@gmail.com>
2024-11-28 19:09:19 -0500

    Manually configured tracing.

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"os"
 )
 
 // The documentation for the Kafka libraries are at https://pkg.go.dev/github.com/confluentinc/confluent-kafka-go/kafka
 func main() {
+
+	tracer.Start(tracer.WithAgentAddr("localhost:8126"),
+		tracer.WithService("curryware-kafka-go-processor"),
+		tracer.WithServiceVersion("1.0.0"),
+		tracer.WithEnv("prod"))
+
+	defer tracer.Stop()
 
 	server := "ubuntu-postgres.curryware.org:9092"
 	topic := "PlayerTopic"

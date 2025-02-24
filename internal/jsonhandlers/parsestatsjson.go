10bed9586d663072f4206e48276c43645a90d9bb
Scot Curry <scotcurry4@gmail.com>
2025-02-24 11:29:20 -0500

    Same change as before for stats.

--- internal/jsonhandlers/parsestatsjson.go
+++ internal/jsonhandlers/parsestatsjson.go
@@ -3,14 +3,18 @@ package jsonhandlers
 import (
 	"curryware-kafka-go-processor/internal/fantasyclasses"
 	"curryware-kafka-go-processor/internal/logging"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 )
 
 func ParseMultipleStatInfo(statsInfo string) []fantasyclasses.StatsInfo {
 
+	decodedBytes, err := base64.StdEncoding.DecodeString(statsInfo)
+	statsInfo = string(decodedBytes)
+
 	var multipleStatInfo fantasyclasses.StatsJson
-	err := json.Unmarshal([]byte(statsInfo), &multipleStatInfo)
+	err = json.Unmarshal([]byte(statsInfo), &multipleStatInfo)
 	if err != nil {
 		fmt.Println("Error parsing player info")
 		logging.LogError("Error parsing player info: ", err)
